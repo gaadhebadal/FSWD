455208568aeafce2ee407e5268173a6fb8ac8f66
gaadhebadal <badalahir124@gmail.com>
2023-06-29 16:15:41 +0530

    adding files

---
+++ week-1/week1_t1.js
@@ -0,0 +1,11 @@
+// Using var
+var name = "Badal"; // string data type
+console.log("Name:", name);
+
+// Using let
+let age = 20; // number data type
+console.log("Age:", age);
+
+// Using const
+const isStudent = true; // boolean data type
+console.log("Is Student:", isStudent);
