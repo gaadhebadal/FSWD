f704a0d8b4fbf24ed98594ac85d60ea1f7d422bb
gaadhebadal <badalahir124@gmail.com>
2023-08-02 16:17:45 +0530

    week-5

--- week-5/task-1/routes/items.js
+++ week-5/task-1/routes/items.js
@@ -27,19 +27,19 @@ let data = [
   },
 ];
 
-// this end-point of an API returns JSON data array
+// Middleware to parse JSON in the request body
+router.use(express.json());
+
+// GET all items
 router.get("/", function (req, res) {
   res.status(200).json(data);
 });
 
-// this end-point returns an object from a data array find by id
-// we get `id` from URL end-points
+// GET an item by ID
 router.get("/:id", function (req, res) {
-  // find an object from `data` array match by `id`
   let found = data.find(function (item) {
     return item.id === parseInt(req.params.id);
   });
-  // if object found return an object else return 404 not-found
   if (found) {
     res.status(200).json(found);
   } else {
@@ -47,18 +47,46 @@ router.get("/:id", function (req, res) {
   }
 });
 
+// POST: Create a new item
 router.post("/", function (req, res) {
-    // Assuming the user sends a JSON object with the "name" property in the request body
-    const { name } = req.body;
-    if (!name) {
-      res.status(400).json({ error: "Name is required." });
-    } else {
-      const newId = data.length > 0 ? data[data.length - 1].id + 1 : 1;
-      const newItem = { id: newId, name: name };
-      data.push(JSON.stringify(newItem));
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ error: "Name is required." });
+  } else {
+    const newId = data.length > 0 ? data[data.length - 1].id + 1 : 1;
+    const newItem = { id: newId, name: name };
+    data.push(newItem);
+    res.status(201).json(newItem);
+  }
+});
 
-      res.status(201).json(newItem);
+// PUT: Update an item by ID
+router.put("/:id", function (req, res) {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ error: "Name is required." });
+  } else {
+    const itemId = parseInt(req.params.id);
+    const foundIndex = data.findIndex((item) => item.id === itemId);
+    if (foundIndex !== -1) {
+      data[foundIndex].name = name;
+      res.status(200).json(data[foundIndex]);
+    } else {
+      res.sendStatus(404);
     }
-  });
+  }
+});
+
+// DELETE: Delete an item by ID
+router.delete("/:id", function (req, res) {
+  const itemId = parseInt(req.params.id);
+  const foundIndex = data.findIndex((item) => item.id === itemId);
+  if (foundIndex !== -1) {
+    const deletedItem = data.splice(foundIndex, 1);
+    res.status(200).json(deletedItem[0]);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 module.exports = router;
